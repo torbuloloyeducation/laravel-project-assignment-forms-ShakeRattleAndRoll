9ebf651507de595668898f21bdad629ad2525733
ShakeRattleAndRoll <k.recones.146983.tc@umindanao.edu.ph>
2026-04-18 17:02:13 +0800

    Form Activity Ans

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome', [
@@ -27,9 +28,49 @@ Route::get('/formtest', function(){
 Route::post('/formtest', function(){
     $email = request('email');
 
+    // check if email is empty
+    if ($email == null) {
+        session()->flash('error-message', 'Email input is invalid!');
+
+        return redirect()->back();
+    };
+
+    request()->validate([
+        'email' => 'email|required' ]);
+
+    $duplicatefinder = session()->get('$emails', []);
+
+    // check if the email save reach the limit of 5
+    if (count($duplicatefinder) >= 5) {
+        session()->flash('error-message', 'Email reach maximum limit 5!');
+    
+        return redirect()->back();
+    }
+
+    // check if the email already exist. if it exist this run
+    if (in_array($email,$duplicatefinder)) {
+        session()->flash('error-message', 'This email already exist!');
+
+        return redirect()->back();
+    };
+
+    session()->flash('message', 'Email added successfully!');
+
     session()->push('$emails', $email);
 
     return redirect('/formtest');
+});
+
+// the delete button route
+Route::post('/delete-email', function() {
+    $id = request('index');
+    $emails = session()->get('$emails', []);
+
+    unset($emails[$id]);
+
+    session()->flash('message', 'Email deleted successfully!');
+    session()->put('$emails', array_values($emails));
+    return redirect() -> back();
 });
 
 Route::get('/delete-emails', function(){
